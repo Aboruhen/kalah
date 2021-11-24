029bad23a01a2cb579127b7108d72a055e0e5e53
r.kniazevych <aboruhen@gmail.com>
2021-11-24 13:52:27 +0200

    add logs;

--- kalah-metrics/src/main/java/com/start/kalah/metrics/MetricController.java
+++ kalah-metrics/src/main/java/com/start/kalah/metrics/MetricController.java
@@ -1,9 +1,11 @@
 package com.start.kalah.metrics;
 
 import java.util.Random;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MetricController {
 
@@ -11,7 +13,9 @@ public class MetricController {
 
     @GetMapping("metric")
     public String testMetric() {
-        return String.valueOf(random.nextDouble());
+        double metric = random.nextDouble();
+        log.info("Get metric: {} from {}", metric, this.getClass());
+        return String.valueOf(metric);
     }
 
 }
